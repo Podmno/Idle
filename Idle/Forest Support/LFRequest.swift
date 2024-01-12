67a3054ff6557c79e21b0f8077ec9a3f2631ccbf
Podmno <podmno@icloud.com>
2024-01-12 15:25:12 +0800

    添加：Forest Link Core

--- Idle/Forest Support/LFRequest.swift
+++ Idle/Forest Support/LFRequest.swift
@@ -5,4 +5,31 @@
 //  Created by Ki MNO on 2024/1/12.
 //
 
-import Foundation
+import Cocoa
+
+public class LFRequest : NSObject {
+    
+    public func userLogin(username: String, password: String, china_account: Bool) {
+        
+        print("LFRequest Login with Username \(username), Password \(password), is_china_account \(china_account)")
+        
+        if !china_account {
+            
+            
+            
+        } else {
+            
+            
+            
+        }
+        
+    }
+    
+    public func getAccountInfo() {
+        
+        
+    }
+    
+    
+    
+}
